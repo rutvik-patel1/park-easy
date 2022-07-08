b93362b16b60af3fa92f7bfa743548a5dc5476c7
Rutvik Patel <rutvik.patel@bacancy.com>
2022-07-08 10:21:03 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      content: {
+        'check': 'url("/src/Image/check.svg")',
+        
+      },
+    },
+  },
+  plugins: [],
+}
